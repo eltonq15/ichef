fac41db374891eed6697927bf5a175632ba2d80b
elton <eltonq15@gmail.com>
2023-01-08 14:39:40 -0300

    add node version to package.json

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 // create a node server with express, cors
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+dotenv.config();
 
 //import the function to fetch data from chatgpt
-const { getAnswerFromChatGPT } = require('./api/chatgpt');
+const { getAnswerFromChatGPT } = require("./api/chatgpt");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // use cors
 app.use(cors());
 
 // create a route
-app.get('/recipe', async (req, res) => {
-    const response = await getAnswerFromChatGPT(req.query.question);
-    res.send(response);
-    }
-);
+app.get("/recipe", async (req, res) => {
+  const response = await getAnswerFromChatGPT(req.query.question);
+  res.send(response);
+});
 
 // start the server
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-    }
-);
+  console.log(`Example app listening at http://localhost:${port}`);
+});
