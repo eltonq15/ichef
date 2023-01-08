ef57fcef9cebde374414b5e36486763c6111c024
elton <eltonq15@gmail.com>
2023-01-08 13:26:39 +0000

    Initial commit

---
+++ src/api/chatgpt/index.js
@@ -0,0 +1,36 @@
+// create function to fetch data from chatgpt with axios and return the response by resolving a promise
+const axios = require('axios');
+
+//import dotenv
+require('dotenv').config();
+
+const getAnswerFromChatGPT = (question) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            method: 'POST',
+            url: 'https://api.openai.com/v1/completions',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': "Bearer " + process.env.CHATGPT_API_KEY,
+            },
+            data: {
+                "model": "text-davinci-002",
+                "prompt": question,
+                "max_tokens": 200,
+            },
+          };
+
+        axios(options)
+        .then((response) => {
+            let answer = response.data?.choices[0]?.text;
+            resolve(('<pre>' + answer + '</pre>')|| "Sorry, I don't know the answer to that question.");
+        })
+        .catch((error) => {
+            reject(error);
+        });
+    });
+}
+
+module.exports = {
+    getAnswerFromChatGPT
+};
