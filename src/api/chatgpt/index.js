f459c8fbb076d115d0a5c4dc1c8a8b6a64d4429b
Elton <eltonq15@gmail.com>
2023-02-15 21:26:20 +0000

    increase  query length

--- src/api/chatgpt/index.js
+++ src/api/chatgpt/index.js
@@ -1,35 +1,35 @@
 // create function to fetch data from chatgpt with axios and return the response by resolving a promise
-const axios = require('axios');
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const getAnswerFromChatGPT = (question) => {
-    return new Promise((resolve, reject) => {
-        const options = {
-            method: 'POST',
-            url: 'https://api.openai.com/v1/completions',
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': "Bearer " + process.env.CHATGPT_API_KEY,
-            },
-            data: {
-                "model": "text-davinci-003",
-                "prompt": question,
-                "max_tokens": 450,
-            },
-          };
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: "POST",
+      url: "https://api.openai.com/v1/completions",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.CHATGPT_API_KEY,
+      },
+      data: {
+        model: "text-davinci-003",
+        prompt: question,
+        max_tokens: 600,
+      },
+    };
 
-        axios(options)
-        .then((response) => {
-            let answer = response.data?.choices[0]?.text;
-            resolve(answer || "Sorry, I don't know the answer to that question.");
-        })
-        .catch((error) => {
-            reject(error);
-        });
-    });
-}
+    axios(options)
+      .then((response) => {
+        let answer = response.data?.choices[0]?.text;
+        resolve(answer || "Sorry, I don't know the answer to that question.");
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
 
 module.exports = {
-    getAnswerFromChatGPT
+  getAnswerFromChatGPT,
 };
